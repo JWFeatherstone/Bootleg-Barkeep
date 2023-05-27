d2aff7bf073d1d9f63e1c34ea03dc4f4710a0045
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-05-27 15:35:14 -0600

    Refactors the App component to a functional component making use of hooks to fetch and set the random drink. Wraps the different routes within a Switch component. Adds some further type checking for the fetched data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
 import Home from "./Components/Home/Home";
 import { fetchRandom } from './Components/API/apiCalls';
 import Header from "./Components/Header/Header";
 import { DrinkGrid } from './Components/DrinkGrid/DrinkGrid';
-import { Route } from 'react-router-dom';
+import { Drink } from './Types/Drink';
+import { Route, Switch } from 'react-router-dom';
 
-interface AppState {
-  randomDrink: any[];
-  error: string;
-  isLoading: boolean;
-}
+const App = () => {
+  const [randomDrink, setRandomDrink] = useState<Drink[]>([]);
+  const [errorMsg, setErrorMsg] = useState<string>('');
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      randomDrink: [],
-      error: '',
-      isLoading: true,
-    };
-  }
+  const getRandomDrink = useCallback(async () => {
+    try {
+      const jsonData = await fetchRandom();
+      setRandomDrink(jsonData.drinks);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMsg(error.message);
+      } else {
+        setErrorMsg("An error occurred");
+      }
+    }
+  }, []);
 
-  componentDidMount(): void {
-    this.getRandomDrink();
-  }
+  useEffect(() => {
+    getRandomDrink();
+  }, [getRandomDrink]);
 
-  getRandomDrink = () => {
-    fetchRandom()
-      .then((jsonData) => {
-        this.setState({ randomDrink: jsonData.drinks, isLoading: false });
-      })
-      .catch((error) => this.setState({ error: error.message }));
-  };
-
-  render() {
-    const { randomDrink } = this.state;
-    return (
-      <main>
-        <Header></Header>
+  return (
+    <main>
+      <Header />
+      <Switch>
         <Route exact path="/">
           <Home randomDrink={randomDrink} />
         </Route>
         <Route exact path="/drinks/:alcohol">
           <DrinkGrid />
         </Route>
-      </main>
-      
-    );
-  }
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
