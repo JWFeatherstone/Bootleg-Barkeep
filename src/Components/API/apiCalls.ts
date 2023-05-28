a97231bd44ce1dc4dcb96693716b28a423bd8c6b
Kelli Watkins <watkinskellil@gmail.com>
2023-05-28 09:01:26 -0600

    Add fecth call for drink detials

--- src/Components/API/apiCalls.ts
+++ src/Components/API/apiCalls.ts
@@ -6,22 +6,35 @@ interface RandomResponse {
 
 const fetchRandom = (): Promise<RandomResponse> => {
   return fetch(`https:www.thecocktaildb.com/api/json/v1/1/random.php`)
-  .then(res => {
-    if(!res.ok){
-      throw new Error(`Please try again there is an error. code: ${res.status}`)
-    } else {
-      return res.json() as Promise<RandomResponse>
-    };
-  });
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`Please try again there is an error. code: ${res.status}`)
+      } else {
+        return res.json() as Promise<RandomResponse>
+      };
+    });
 };
 
 const fetchCocktails = async (alcohol: string): Promise<Drink[]> => {
   const response = await fetch(`https:www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`);
-  if(response.ok){
+  if (response.ok) {
     return (await response.json()).drinks;
   } else {
     throw new Error(`Please try again there is an error. code: ${response.status}`)
   };
 }
 
-export { fetchRandom, fetchCocktails }
+const fetchDetails = (id: string): Promise<Drink[]> => {
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(`Please try again there is an error. code: ${res.status}`)
+      } else {
+        return res.json()
+      }
+    })
+}
+
+
+
+export { fetchRandom, fetchCocktails, fetchDetails }
