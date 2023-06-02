ac014f3fdadfa28b36877263e149bd590566cc91
John Featherstone <johnwfeatherstone.personal@gmail.com>
2023-06-02 09:11:53 -0600

    Fix small alignment issues between header and drink grid. Merge titles for grid pages.

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,16 +7,15 @@ const Header: React.FC = () => {
   return (
     <header>
       <div className="icon-border">
-        <div className="icon-backer"></div>
         <NavLink to="/" className="home-nav"></NavLink>
       </div>
       <nav>
-        <NavLink to="/drinks/scotch" className="ingredient-nav first-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>SCOTCH</NavLink>
-        <NavLink to="/drinks/brandy" className="ingredient-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>BRANDY</NavLink>
-        <NavLink to="/drinks/vodka" className="ingredient-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>VODKA</NavLink>
-        <NavLink to="/drinks/gin" className="ingredient-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>GIN</NavLink>
-        <NavLink to="/drinks/rum" className="ingredient-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>RUM</NavLink>
-        <NavLink to="/drinks/tequila" className="ingredient-nav" style={{ color: 'inherit', textDecoration: 'inherit'}}>TEQUILA</NavLink>
+        <NavLink to="/drinks/scotch" className="ingredient-nav first-nav" style={{textDecoration: 'inherit'}}>SCOTCH</NavLink>
+        <NavLink to="/drinks/brandy" className="ingredient-nav" style={{textDecoration: 'inherit'}}>BRANDY</NavLink>
+        <NavLink to="/drinks/vodka" className="ingredient-nav" style={{textDecoration: 'inherit'}}>VODKA</NavLink>
+        <NavLink to="/drinks/gin" className="ingredient-nav" style={{textDecoration: 'inherit'}}>GIN</NavLink>
+        <NavLink to="/drinks/rum" className="ingredient-nav" style={{textDecoration: 'inherit'}}>RUM</NavLink>
+        <NavLink to="/drinks/tequila" className="ingredient-nav" style={{textDecoration: 'inherit'}}>TEQUILA</NavLink>
       </nav>
     </header>
   );
