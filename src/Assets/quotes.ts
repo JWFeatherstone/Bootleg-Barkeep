cdab340dc74f9c1048203c05e567f3e10308876d
Adriane Sutherland <asutherland91@gmail.com>
2023-06-02 07:28:40 -0700

    Add new assets and update css styling, also add a random quote component to the home page

---
+++ src/Assets/quotes.ts
@@ -0,0 +1,58 @@
+export const quotes = [
+  {
+    quote: "Prohibition has made nothing but trouble.",
+    author: "Al Capone"
+  },
+  {
+    quote: "I have always believed that prohibition was doomed to failure.",
+    author: "Warren G. Harding"
+  },
+  {
+    quote: "Prohibition didn't work in the Garden of Eden. Adam ate the apple",
+    author: "Vicente Fox"
+  },
+  {
+    quote: "Once during prohibition, I was forced to live for days on nothing but food and water",
+    author: "W. C Fields"
+  },
+  {
+    quote: "For every prohibition you create, you also create an underground",
+    author: "Jello Biafra"
+  },
+  {
+    quote: "They didn't like the Prohibitionists, and in the great cities, the police were often with the bootleggers. The politicians were even on the payroll.",
+    author: "Mark Hellinger"
+  },
+  {
+    quote: "I don't know what effect Prohibition has on other people, but in my case, it fills me with dismay.",
+    author: "Noel Coward"
+  },
+  {
+    quote: "What America needs now is a drink",
+    author: "Franklin D. Roosevelt"
+  },
+  {
+    quote: "The only thing that whiskey's ever done for me is it gave me the strength to finish the rest of the bottle!",
+    author: "Henny Youngman"
+  },
+  {
+    quote: "When I sell liquor, it's bootlegging. When my patrons serve it on Lake Shore Drive, it's hospitality.",
+    author: "Al Capone"
+  },
+  {
+    quote: "Why don't the pass a consititutional amendment prohibity anybody from learning anything? If it works as well as prohibition did, in five years Americans would be the smartest race of people on Earth.",
+    author: "Will Rogers"
+  },
+  {
+    quote: "Prohibition makes you want to cry into your beer and denies you the beer to cry into",
+    author: "Don Marquis"
+  },
+  {
+    quote: "We find many things to which the prohibition of them consistures the only temptation",
+    author: "William Hazlitt"
+  },
+  {
+    quote: "Prohibition is better than no liquor at all",
+    author: "Will Rogers"
+  }
+];
