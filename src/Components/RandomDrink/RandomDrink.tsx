657da65456df9a981d3d8671c12a240b3e2d948a
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-06-02 10:46:16 -0400

    Updated random drink to nav link so user can be taken to the detailed view. Began trouble shooting the timeout on the cypress error test more work needed.

--- src/Components/RandomDrink/RandomDrink.tsx
+++ src/Components/RandomDrink/RandomDrink.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import "../RandomDrink/RandomDrink.css";
 import { Drink } from "src/Types/Drink";
+import { NavLink } from "react-router-dom";
 
-const RandomDrink: React.FC<Drink> = ({ strDrink, strDrinkThumb, idDrink }) => {
+const RandomDrink: React.FC<Drink> = ({ strDrink, strDrinkThumb, idDrink:id }) => {
   return (
-    <>
+    <NavLink className="random-card-nav" to={`/drink/${id}`} key={id} >
     <div className="random-page-wrapper">
     <div className="lucky-line">
     <h2 className="lucky">Lucky Libations</h2>
     </div>
       <div className="random-container">
-        <img className="random-img" src={strDrinkThumb} alt={strDrink} id={idDrink} />
-        
+        <img className="random-img" src={strDrinkThumb} alt={strDrink} id={id} />
         <h2 className="title title-two">{strDrink}</h2>
-       
       </div>
       </div>
-    </>
+    </NavLink >
   );
 };
 
