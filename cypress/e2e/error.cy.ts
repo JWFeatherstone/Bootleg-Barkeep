f1e3e2782a24dd97f1194147b6565932641d2e6c
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-06-03 18:56:14 -0400

    Add formatting to cypress test

--- cypress/e2e/error.cy.ts
+++ cypress/e2e/error.cy.ts
@@ -1,41 +1,60 @@
-describe('error handling', () => {
+describe("error handling", () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v1/1/random.php', {
-      statusCode: 500
-    })
+    cy.intercept(
+      "GET",
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+      {
+        statusCode: 500,
+      }
+    );
 
-    cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin', {
-      statusCode: 500
-    })
+    cy.intercept(
+      "GET",
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=gin",
+      {
+        statusCode: 500,
+      }
+    );
 
-    cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=15346', {
-      statusCode: 500
-    })
-  })
+    cy.intercept(
+      "GET",
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=15346",
+      {
+        statusCode: 500,
+      }
+    );
+  });
 
-  it('should display an error page when a randomized drink fails to fetch for the home page', () => {
-    cy.visit('localhost:3000')
-    cy.get('section').should('have.class', 'error-page')
-    cy.get('.error-message').should('have.text', 'A server error occured while we were trying to fetch your cocktails. Please try again.')
-  })
-  
-  it('should display an error page when the cocktails for the drink grid fail to fetch', () => {
-    cy.visit('localhost:3000') 
-    cy.visit('localhost:3000/drinks/gin')
-    cy.get('section').should('have.class', 'error-page')
-    cy.get('.error-message').should('have.text', 'A server error occured while we were trying to fetch your cocktails. Please try again.')
-    cy.wait(2000) 
-    cy.url().should('eq', 'http://localhost:3000/error')
-  })
-  
-  it('should display an error page when the details for a specific cocktail fail to fetch', () => {
-    cy.visit('localhost:3000') 
-    cy.visit('localhost:3000/drinks/15346') 
-    cy.get('section').should('have.class', 'error-page')
-    cy.get('.error-message').should('have.text', 'A server error occured while we were trying to fetch your cocktails. Please try again.')
-    cy.wait(2000)
-    cy.url().should('eq', 'http://localhost:3000/error')
-  })
-})
+  it("should display an error page when a randomized drink fails to fetch for the home page", () => {
+    cy.visit("localhost:3000");
+    cy.get("section").should("have.class", "error-page");
+    cy.get(".error-message").should(
+      "have.text",
+      "A server error occured while we were trying to fetch your cocktails. Please try again."
+    );
+  });
 
+  it("should display an error page when the cocktails for the drink grid fail to fetch", () => {
+    cy.visit("localhost:3000");
+    cy.visit("localhost:3000/drinks/gin");
+    cy.get("section").should("have.class", "error-page");
+    cy.get(".error-message").should(
+      "have.text",
+      "A server error occured while we were trying to fetch your cocktails. Please try again."
+    );
+    cy.wait(2000);
+    cy.url().should("eq", "http://localhost:3000/error");
+  });
 
+  it("should display an error page when the details for a specific cocktail fail to fetch", () => {
+    cy.visit("localhost:3000");
+    cy.visit("localhost:3000/drinks/15346");
+    cy.get("section").should("have.class", "error-page");
+    cy.get(".error-message").should(
+      "have.text",
+      "A server error occured while we were trying to fetch your cocktails. Please try again."
+    );
+    cy.wait(2000);
+    cy.url().should("eq", "http://localhost:3000/error");
+  });
+});
--- cypress/e2e/random.cy.ts
+++ cypress/e2e/random.cy.ts
@@ -1,27 +1,40 @@
-describe('RandomDrink Component', () => {
+describe("RandomDrink Component", () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://www.thecocktaildb.com/api/json/v1/1/random.php', {
-      statusCode: 200,
-      body: {
-        drinks: [
-          {
-            "strDrink": "155 Belmont",
-            "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg",
-            "idDrink": "15346",
-          },
-        ],
-      },
-    }).as('randomDrink');
+    cy.intercept(
+      "GET",
+      "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+      {
+        statusCode: 200,
+        body: {
+          drinks: [
+            {
+              strDrink: "155 Belmont",
+              strDrinkThumb:
+                "https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg",
+              idDrink: "15346",
+            },
+          ],
+        },
+      }
+    ).as("randomDrink");
 
-    cy.visit('localhost:3000'); 
+    cy.visit("localhost:3000");
   });
 
-  it('should display a random drink with title and image', () => {
-    cy.wait('@randomDrink')
+  it("should display a random drink with title and image", () => {
+    cy.wait("@randomDrink")
 
-    .get('h2').should('contain', 'LUCKY LIBATIONS')
-    .get('.random-img').should('have.attr', 'src', 'https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg')
-    .get('.random-img').should('have.attr', 'alt', '155 Belmont')
-    .get('.title').should('contain', '155 Belmont');
+      .get("h2")
+      .should("contain", "LUCKY LIBATIONS")
+      .get(".random-img")
+      .should(
+        "have.attr",
+        "src",
+        "https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg"
+      )
+      .get(".random-img")
+      .should("have.attr", "alt", "155 Belmont")
+      .get(".title")
+      .should("contain", "155 Belmont");
   });
 });
